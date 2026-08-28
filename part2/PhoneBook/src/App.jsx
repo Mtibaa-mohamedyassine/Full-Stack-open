99174afab0f7fe6f32310cdb2c6ec6fff3ecf511
Mtibaa-mohamedyassine <mtibaamohamedyassine@gmail.com>
2026-08-28 15:43:10 +0100

    Add files via upload

---
+++ part2/PhoneBook/src/App.jsx
@@ -0,0 +1,82 @@
+import { useState, useEffect } from "react"
+import Persons from './components/Persons'
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+import BackendServices from './Services/BackendServices'
+
+
+const App = () => {
+  const [persons, setPersons] = useState([])
+
+  const [newName, setNewName] = useState('')
+  const [newPhone, setNewPhone] = useState('')
+  const [newQuery, setNewQuery] = useState('')
+
+  const handleNewChange = (event) => setNewName(event.target.value)
+  const handleChangePhone = (event) => setNewPhone(event.target.value)
+  const handleChangeQuery = (event) => setNewQuery(event.target.value)
+
+    
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    
+    const np = {name: newName, number:newPhone}
+
+    const Exist = persons.some(person => newName === person.name)
+
+    if(!Exist)
+    {
+      BackendServices
+        .addToDb(np)
+        .then(returnedPerson => {
+          setPersons(persons.concat(returnedPerson))
+          setNewName('')
+          setNewPhone('')
+          console.log('pushed to the server succesfully!')
+          console.log(returnedPerson)
+        })
+    }
+    else
+      alert(`${newName} is already added to PhoneBook`)
+  }
+
+  useEffect(() => {
+    console.log('1st load')
+
+    BackendServices
+      .getAll()
+      .then(data => {
+        setPersons(data)
+        console.log('promise fulfilled')
+        console.log(data)
+      })
+  }, [])
+
+  const personsToShow = persons.filter((contact) => (contact.name.toLowerCase().includes(newQuery.toLowerCase())))
+  
+  
+  return(
+    <div>
+      <h1>PhoneBook</h1>
+     
+      <Filter handleChangeQuery={handleChangeQuery} />
+
+      <h1>Add a new</h1>
+      
+      <PersonForm addPerson={addPerson} 
+                  handleNewChange={handleNewChange} 
+                  handleChangePhone={handleChangePhone} 
+                  newName={newName}
+                  newPhone={newPhone} />
+
+      <h1>Numbers</h1>
+      
+      <Persons personsToShow={personsToShow} />
+
+    </div>
+  )
+}
+
+
+export default App

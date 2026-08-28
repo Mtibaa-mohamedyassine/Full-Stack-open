3b0979100976c8b144b4a8d38c0102617a57f026
Mtibaa-mohamedyassine <mtibaamohamedyassine@gmail.com>
2026-08-28 15:47:39 +0100

    Add files via upload

---
+++ part2/PhoneBook/src/Services/BackendServices.jsx
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+
+
+const baseUrl = 'http://localhost:3001/persons'
+
+const getAll = () => {
+
+    const request = axios.get(baseUrl)
+    return request.then(response => (response.data))                       
+}
+
+
+const addToDb = (newObject) => {
+
+    const request = axios.post(baseUrl, newObject)
+    return request.then(response => (response.data))
+}
+
+
+export default {getAll, addToDb}

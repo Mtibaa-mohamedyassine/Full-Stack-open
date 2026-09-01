cb91fb3287e93336250aaf3bffdad8486b1ef372
Mtibaa-mohamedyassine <mtibaamohamedyassine@gmail.com>
2026-09-01 17:56:26 +0100

    Add files via upload

--- part2/Data for countries/src/services/services.jsx
+++ part2/Data for countries/src/services/services.jsx
@@ -1,1 +1,18 @@
+import axios from 'axios'
 
+const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/api/all'
+
+const getData = () => {
+  return axios
+    .get(baseUrl)
+    .then(response => {
+      console.log('request fulfilled')
+      return response.data
+    })
+    .catch(error => {
+      console.log('request failed:', error)
+      throw error
+    })
+}
+
+export default { getData }

0959d1c243600dfbe50133a32b809a209f08f593
Mtibaa-mohamedyassine <mtibaamohamedyassine@gmail.com>
2026-09-02 15:33:52 +0100

    Add files via upload

--- part2/PhoneBook/src/App.jsx
+++ part2/PhoneBook/src/App.jsx
@@ -1,150 +1,85 @@
-import { useState, useEffect } from "react"
-import Persons from './components/Persons'
-import Filter from './components/Filter'
-import PersonForm from './components/PersonForm'
-import Notification from "./components/Notification"
-import BackendServices from './Services/BackendServices'
-
+import { useEffect, useState } from "react";
+import services from './services/services.jsx'
 
 const App = () => {
-  const [persons, setPersons] = useState([])
 
-  const [newName, setNewName] = useState('')
-  const [newPhone, setNewPhone] = useState('')
-  const [newQuery, setNewQuery] = useState('')
-  const [newNotification, setNotification] = useState(null)
-  const [notificationStatus, setNotificationStatus] = useState('')
+  const [country, setCountry] = useState('')
+  const [data, setData] = useState([])
 
-
-  const handleNewChange = (event) => setNewName(event.target.value)
-  const handleChangePhone = (event) => setNewPhone(event.target.value)
-  const handleChangeQuery = (event) => setNewQuery(event.target.value)
-
-  const handleDelete = (objectId) => {
-    const person = persons.find(p => p.id == objectId)
-    if(!person) return
-
-    if(window.confirm(`Shure to delete ${person.name}`))
-      BackendServices
-        .deleteFromDb(objectId)
-        .then(person => {
-          setPersons(persons.filter(p => p.id != objectId))
-          setNotificationStatus('info')
-          setNotification(`person with this id: ${objectId} deleted form the server`)
-          setTimeout(() => {
-            setNotification(null)
-          },5000)
-        })
-        .catch(error => {
-          console.log(`failed to delete from the server the person with this id: ${objectId}`)
-          setNotificationStatus('error')
-          setNotification(`failed to delete from the server the person with this id: ${objectId}`)
-          setTimeout(() => {
-            setNotification(null)
-          },5000)
-        })
-  }
-    
-
-  const addPerson = (event) => {
-    event.preventDefault()
-    
-    const np = {name: newName, number:newPhone}
-
-    const existPerson = persons.find(person => newName === person.name)
-
-    if(!existPerson)
-    {
-      BackendServices
-        .addToDb(np)
-        .then(returnedPerson => {
-          setPersons(persons.concat(returnedPerson))
-          setNewName('')
-          setNewPhone('')
-          console.log('pushed to the server succesfully!')
-          console.log(returnedPerson)
-          setNotificationStatus('info')
-          setNotification(`Added ${np.name}`)
-          setTimeout(() => {
-                    setNotification(null)
-          }, 5000)
-        })
-    }
-    else if(existPerson && np.number != existPerson.number)
-    {
-      if(window.confirm(`${np.name} exist, do you want to replace the old number by ${np.number}`))
-      {
-        BackendServices
-          .updateDb(np, existPerson.id)
-          .then((resonse => {
-            console.log(`${np.name} updated successfully!`)
-            const newPersonsList = persons.map(person => person.name === np.name ? {...person, number: np.number}: person)
-            setPersons(newPersonsList)
-            setNotificationStatus('info')
-            setNotification(`${np.name} is updated`)
-            setTimeout(() => {
-              setNotification(null)
-            }, 5000)
-          }))
-          .catch(() => {
-            setNotificationStatus('error')
-            setNotification(`Information of ${np.name} has already been romoved from server`)
-            setTimeout(() => {
-              setNotification(null)
-            },5000)
-          })
-      }
-    }
-    else
-    {
-      setNotificationStatus('info')
-      setNotification(`${newName} is already added to PhoneBook`)
-      setTimeout(() => {
-        setNotification(null)
-      }, 5000)
-    }
-    
-
+  const handleChange = (event) => {
+    setCountry(event.target.value)
   }
 
   useEffect(() => {
-    console.log('1st load')
+       services
+        .getData(country)
+        .then(response => {
+          //console.log(response)
+          setData(response)  
+        })
+        .catch(error => console.log(error))
+    } 
+  ,[country])
+ 
+  const countriesToShow = 
+    country.trim() === '' ? [] : data.filter((c) => (
+      c.name.common.toLowerCase().includes(country.toLowerCase())
+    ))
 
-    BackendServices
-      .getAll()
-      .then(data => {
-        setPersons(data)
-        console.log('promise fulfilled')
-        console.log(data)
-      })
-  }, [])
-
-  const personsToShow = persons.filter((contact) => (contact.name.toLowerCase().includes(newQuery.toLowerCase())))
+  const handleShowCountry = (name) => {
+    setCountry(name)
+  }
   
-  
-  return(
+  return (
+  <div>
+    find countries <input onChange={handleChange} value={country}/>
     <div>
-      <h1>PhoneBook</h1>
-     
-      <Filter handleChangeQuery={handleChangeQuery} />
+      {country.trim() !== '' && countriesToShow.length > 10 && 
+        <p>Too many matches, specify another filter</p> }
 
-      <Notification message={newNotification} status={notificationStatus} />
+      {countriesToShow.length >1 && countriesToShow.length <= 10 && (
+        <div>
+          {countriesToShow.map(c => (
+            <div key={c.cca3 || c.name.common}>
+              <span>{c.name.common} </span>
+              <button onClick={() => handleShowCountry(c.name.common)}>Show</button>
+            </div>
+          ))}
+        </div> 
+      )}
+        
 
-      <h1>Add a new</h1>
-      
-      <PersonForm addPerson={addPerson} 
-                  handleNewChange={handleNewChange} 
-                  handleChangePhone={handleChangePhone} 
-                  newName={newName}
-                  newPhone={newPhone} />
+      {countriesToShow.length === 1 && (
+        <div>
+            <h2>{countriesToShow[0].name.common}</h2>
+            <div>{countriesToShow[0].capital && countriesToShow[0].capital.length > 0 
+                ? countriesToShow[0].capital[0] 
+                : 'N/A'}
+            </div>
+            <div>
+              Area {countriesToShow[0].area}
+            </div>
 
-      <h1>Numbers</h1>
-      
-      <Persons personsToShow={personsToShow} handleDelete={handleDelete} />
+            <h4>languages:</h4>
+            <ul>
+              {Object.values(countriesToShow[0].languages || {}).map(lang => (
+              <li key={lang}>{lang}</li>
+            ))}
+            </ul>
 
+            <img
+              src={countriesToShow[0].flags.png}
+              alt={`Flag of ${countriesToShow[0].name.common}`}
+              width="150"
+            />
+            
+        </div>
+      )}
     </div>
-  )
-}
+    
+  </div>
+  
+)}
 
 
 export default App
